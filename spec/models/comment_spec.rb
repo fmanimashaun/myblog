476d227c6cae968d686874158c4dde9076651309
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-02 21:39:45 +0100

    Update comment model unit test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:comment) { build(:comment, user:, post:) }
+
+  describe 'associations' do
+    it 'belongs to user' do
+      expect(comment.user).to eq(user)
+    end
+
+    it 'belongs to post' do
+      expect(comment.post).to eq(post)
+    end
+  end
+
+  describe 'validations' do
+    it 'validates presence of text' do
+      comment.text = nil
+      expect(comment).not_to be_valid
+      expect(comment.errors[:text]).to include("can't be blank")
+    end
+  end
 end
