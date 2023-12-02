dbf6e124cbc50d194508c2c9ae604077fc324fa0
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-02 08:44:36 +0100

    Update the associate relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  validates :title, presence: true
+  validates :text, presence: true
 end
