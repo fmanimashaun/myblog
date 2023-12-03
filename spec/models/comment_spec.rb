de46014cf451b5001a3503b0ab4eb4aa1beb9a9f
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-03 01:42:38 +0100

    Update the comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe Comment, type: :model do
       expect(comment.errors[:text]).to include("can't be blank")
     end
   end
+
+  describe 'counter_cache' do
+    let(:post) { create(:post) }
+    let(:comment) { create(:comment, post:) }
+
+    context 'when a comment is created' do
+      it 'increments comments_counter on post' do
+        expect do
+          comment
+        end.to change { post.reload.comments_counter }.by(1)
+      end
+    end
+
+    context 'when a comment is destroyed' do
+      it 'decrements comments_counter on post' do
+        comment
+        expect do
+          comment.destroy
+        end.to change { post.reload.comments_counter }.by(-1)
+      end
+    end
+  end
 end
