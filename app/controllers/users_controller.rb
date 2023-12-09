5d3315eaed39764423061c69215ee73c6274ccef
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-10 00:07:49 +0100

    Update the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
   def index
-    # placeholder for index logic
-    @users = User.all
-    puts @users
+    @users = User.order(:id)
   end
 
   def show
     @user = User.find_by(id: params[:id])
-    puts params[:id]
     flash[:alert] = 'User not found' if @user.nil?
   end
 end
