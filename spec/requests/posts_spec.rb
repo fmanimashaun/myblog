97a6fd800c43172056eb1c7ceea69bb875d67a73
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-09 19:42:16 +0100

    Update user and post controller unit test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,31 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   let(:user) { create(:user) }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get user_posts_path(user)
+  describe 'GET /index' do
+    before { get user_posts_path(user) }
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "includes the correct placeholder text" do
-      get user_posts_path(user)
-      expect(response.body).to include("All Posts Page")
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text' do
+      expect(response.body).to include('All Posts Page')
     end
   end
 
-  describe "GET /show" do
+  describe 'GET /show' do
     let(:post) { create(:post, author: user) }
+    before { get user_post_path(user, post) }
 
-    it "returns http success" do
-      get user_post_path(user, post)
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "includes the correct placeholder text" do
-      get user_post_path(user, post)
-      expect(response.body).to include("Single Post Page")
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text' do
+      expect(response.body).to include('Single Post Page')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,29 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get users_path
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before { get users_path }
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "includes the correct placeholder text" do
-      get users_path
-      expect(response.body).to include("All Users Page")
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text' do
+      expect(response.body).to include('All Users Page')
     end
   end
 
-  describe "GET /show" do
+  describe 'GET /show' do
     let(:user) { create(:user) }
+    before { get user_path(user) }
 
-    it "returns http success" do
-      get user_path(user)
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "includes the correct placeholder text" do
-      get user_path(user)
-      expect(response.body).to include("Single User Page")
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text' do
+      expect(response.body).to include('Single User Page')
     end
   end
 end
