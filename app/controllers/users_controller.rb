93e2a7e9b22880f05b53a22cb4aabd1ffc43fb06
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-09 23:00:01 +0100

    Update the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    puts params[:id]
     flash[:alert] = 'User not found' if @user.nil?
   end
 end
