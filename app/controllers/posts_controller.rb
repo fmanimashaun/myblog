686a30ff3778816ad3d7d067e91d54f22801c7be
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-10 02:07:12 +0100

    Update the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,20 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = @post.author
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
