422dcf35440b51ad71206f003f5e7f91b838834a
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-10 04:17:36 +0100

    Fix linter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,9 +3,9 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = @post.likes.build(user: current_user)
     if @like.save
-      redirect_to user_post_path(@post.user, @post), notice: "Liked!"
+      redirect_to user_post_path(@post.user, @post), notice: 'Liked!'
     else
-      redirect_to user_post_path(@post.user, @post), alert: "Unable to like."
+      redirect_to user_post_path(@post.user, @post), alert: 'Unable to like.'
     end
   end
 end
