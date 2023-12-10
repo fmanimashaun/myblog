cd708147e3da28501133e7389b4b4a4162cb499d
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-10 04:17:01 +0100

    update like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @like = @post.likes.build(user: @user)
-
+    @like = @post.likes.build(user: current_user)
     if @like.save
-      redirect_to user_post_path(@user, @post), notice: 'Liked!'
+      redirect_to user_post_path(@post.user, @post), notice: "Liked!"
     else
-      redirect_to user_post_path(@user, @post), alert: 'Unable to like.'
+      redirect_to user_post_path(@post.user, @post), alert: "Unable to like."
     end
   end
 end
