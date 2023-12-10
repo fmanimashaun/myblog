b279f67e2752a002967e66eac67b11c7fbe069b7
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-10 08:45:52 +0100

    Fixed the routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+  # set the root of the site to the index action of the users controller
+  root "users#index"
+
+  # redirect /users to the root of the site
+  get "/users", to: redirect("/")
+
+  # redirect /users/:id to the show action of the users controller
+  resources :users, only: [:show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
       resources :likes, only: [:create]
     end
   end
