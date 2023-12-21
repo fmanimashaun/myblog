476f17b4c99b92954f0e9b5851f561fd15ad6e82
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-21 23:11:14 +0100

    Update controller unit test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     before { get users_path }
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'renders the correct template' do
-      expect(response).to render_template(:index)
+    it 'redirects to the root path' do
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(root_path)
     end
   end
 
