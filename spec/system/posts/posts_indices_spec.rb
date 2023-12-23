5080c129d9a01af829379c06393c638a69ad3eb0
gdvl777 <gdvl777@users.noreply.github.com>
2023-12-23 09:03:35 -0500

    Update posts_indices_spec.rb

---
+++ spec/system/posts/posts_indices_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+RSpec.describe 'UserPostsIndex', type: :system do
+  before do
+    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    create_list(:post, 10, author: @user) # Create 10 posts for pagination
+  end
+  it 'displays user information and posts correctly' do
+    visit user_posts_path(@user)
+    # User information
+    expect(page).to have_css("img[src*='#{@user.photo}']")
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user.posts_counter)
+    # Post display
+    @user.posts.each do |post|
+      expect(page).to have_content(post.title)
+      expect(page).to have_content(post.text.truncate(250)) # Check partial body
+      expect(page).to have_content(post.comments_counter)
+      expect(page).to have_content(post.likes_counter)
+      # Check first comment if present
+      expect(page).to have_content(post.recent_comments.first.text) unless post.recent_comments.empty?
+    end
+    # Pagination
+    expect(page).to have_css('nav.pagination')
+  end
+  it 'redirects to the post show page when a post is clicked' do
+    visit user_posts_path(@user)
+    first_post = @user.posts.order(created_at: :desc).first
+    click_on first_post.title, match: :first
+    expect(page).to have_current_path(user_post_path(@user, first_post))
+  end
+end
