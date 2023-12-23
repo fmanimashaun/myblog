043ab89fb7ae6427853f709c13ddc4339156f8fc
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-12-23 19:12:11 +0100

    Update authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'author_id', inverse_of: 'author', dependent: :destroy
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+  has_many :posts,
+           foreign_key: "author_id",
+           inverse_of: "author",
+           dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0
+            }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
