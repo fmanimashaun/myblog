72f56497bc347a117d837ed14d12dd21068dee97
gdvl777 <gdvl777@users.noreply.github.com>
2023-12-23 08:51:00 -0500

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.sort_by(&:created_at).reverse.take(5)
   end
 end
